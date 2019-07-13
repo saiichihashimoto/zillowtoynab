e6bfb5a4e58b0291d68302c32ce15412d94c46a7
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-07-13 01:10:15 -0700

    feat(cron): cron job!

--- src/sync.js
+++ src/sync.js
@@ -2,18 +2,24 @@ import { getAccountAmount, postAdjustment } from './ynab';
 import { getZestimate } from './zillow';
 
 export default async function sync({ accessToken, budgetId, accountId, zpId, zwsId }) {
-	const [previousAmount, { amount: zestimate, date }] = await Promise.all([
-		getAccountAmount({ accessToken, budgetId, accountId }),
-		getZestimate({ zpId, zwsId }),
-	]);
+	try {
+		const result = await Promise.all([
+			getAccountAmount({ accessToken, budgetId, accountId }),
+			getZestimate({ zpId, zwsId }),
+		]);
 
-	return postAdjustment({
-		accessToken,
-		budgetId,
-		accountId,
-		zestimate,
-		previousAmount,
-		date,
-		zpId,
-	});
+		const [previousAmount, { amount: zestimate, date }] = result;
+
+		return postAdjustment({
+			accessToken,
+			budgetId,
+			accountId,
+			zestimate,
+			previousAmount,
+			date,
+			zpId,
+		});
+	} catch (err) {
+		throw err;
+	}
 }
