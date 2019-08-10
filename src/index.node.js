c280a4156fa0e8e9deb1dfb1731d6b6349f4e831
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2019-08-10 13:53:46 -0700

    feat(app): using feathers

--- src/index.node.js
+++ src/index.node.js
@@ -1,15 +1,47 @@
 /* istanbul ignore file */
-import express from 'express';
+import { promisify } from 'util';
 
+import express from '@feathersjs/express';
+import { FeathersError } from '@feathersjs/errors';
+
+import app from './app';
 import log from './log';
 
-const port = process.env.PORT;
+const port = app.get('port');
 
-const app = express();
+log.info({ port }, 'Server Starting');
 
-app.get('/', (req, res) => {
-	res.send('hello world');
-	log.info({ req, res }, 'hello world!');
+const server = app
+	.use(express.notFound({ verbose: true }))
+	.use(express.errorHandler({
+		logger: {
+			error: (err) => (
+				!(err instanceof FeathersError) || err.code >= 500
+					? log.error(err)
+					: log.warn(err)
+			),
+		},
+	}))
+	.listen(port, () => log.info({ port }, 'Server Started'));
+
+const close = promisify(server.close.bind(server));
+
+process.once('SIGUSR2', async () => {
+	log.info({ port }, 'Server Stopping');
+
+	await close();
+
+	log.info({ port }, 'Server Stopped');
+
+	process.kill(process.pid, 'SIGUSR2');
 });
 
-app.listen(port, () => log.info({ port }, 'Ready for requests'));
+process.on('unhandledRejection', (reason) => {
+	log.fatal(reason);
+	process.exit(1);
+});
+
+process.on('uncaughtException', (err) => {
+	log.fatal(err);
+	process.exit(1);
+});
--- src/log.js
+++ src/log.js
@@ -1,9 +1,10 @@
-/* istanbul ignore file */
 import bunyan from 'bunyan';
 
 import { name } from '../package';
 
-export default bunyan.createLogger({
+const log = bunyan.createLogger({
 	name,
 	serializers: bunyan.stdSerializers,
 });
+
+export default log;
